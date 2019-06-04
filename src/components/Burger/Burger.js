9f1f71dc1a9130f0dde29bd6b0e7bf1ec4d3439c
Disher Buttar <buttar.dilsher@gmail.com>
2019-06-04 12:13:30 -0700

    adds logic to display the ingredients depending upon the state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,28 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const burger = () => {
+const burger = (props) => {
+    //this const is an array of react elements, we can display
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((name,i) => {
+                return <BurgerIngredient key = {igKey + i} type = {igKey}/>
+            })
+        })
+        .reduce( (acc, curr ) => {
+            return acc.concat(curr);
+        },[]);
+    //used reduce to flatten the array
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
+   
     return (
         <div className = {classes.Burger}>
             <BurgerIngredient type = "bread-top"/>
-            <BurgerIngredient type = "cheese"/>
-            <BurgerIngredient type = "meat"/>
+            {transformedIngredients}
             <BurgerIngredient type = "bread-bottom"/>
+           
         </div>
     )
 }
