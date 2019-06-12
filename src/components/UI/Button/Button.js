160892460e06bb45f56ab5d79261407f30c64aa7
Disher Buttar <buttar.dilsher@gmail.com>
2019-06-12 11:27:25 -0700

    Adds custom button jsx element

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import classes from './Button.css'
 
 const button = (props) => {
     return (
-        <div>
-            
-        </div>
+        <button 
+        className = {[classes.Button, classes[props.btnType]].join(' ')}
+        onClick = {props.clicked}
+        >
+            {props.children}
+        </button>
     )
 }
 
