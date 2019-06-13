36d9b9b8d19541f58e413eb6d24bfa5e31885035
Disher Buttar <buttar.dilsher@gmail.com>
2019-06-13 14:56:08 -0700

    adds 3 lines icon to the sidebar button

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import classes from './DrawerToggle.css';
 
 function drawerToggle(props) {
     return (
-        <div onClick = {props.clicked}>
-            MENU
+        <div onClick = {props.clicked} className = {classes.DrawerToggle}>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     )
 }
