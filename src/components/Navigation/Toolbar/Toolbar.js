1942dd5544f82725133053e631b619c384b33db3
Disher Buttar <buttar.dilsher@gmail.com>
2019-06-12 20:08:51 -0700

    Adds navigation component which will have the nav and toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './Toolbar.css'
+
+function toolbar(props) {
+    return (
+        <header className = {classes.Toolbar}>
+            <div>MENU</div>
+            <div>LOGO</div>
+            <nav>
+
+            </nav>  
+        </header>
+    )
+}
+
+export default toolbar
