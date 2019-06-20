e007b518a43f697015ce9e2cd64bb3f07d3ff82a
Disher Buttar <buttar.dilsher@gmail.com>
2019-06-20 14:56:52 -0700

    Handles the post request on Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Aux from "../../hoc/Aux";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from '../../axios-orders';
 
 const INGRIDIENT_PRICES = {
   salad: 0.5,
@@ -84,7 +85,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('you continued')
+    // alert('you continued')
+    const order = {
+      ingredients : this.state.ingredients,
+      price : this.state.totalPrice,
+      customer : {
+        name: 'Dilsher Buttar',
+        address: {
+          street: '500 El Camino Real',
+          zipCode: 94010,
+          country: 'USA'
+        },
+        email: 'buttar.dilsher@gmail.com'
+      },
+      delivery: 'fastest'
+    }
+    axios.post('/orders.json',order)
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
   }
 
   render() {
