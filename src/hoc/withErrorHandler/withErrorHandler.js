294ce5bffad5eb82c189ede405056e47c81001c5
Disher Buttar <buttar.dilsher@gmail.com>
2019-06-21 14:37:37 -0700

    removes old interceptors in axios to prevent memory leak

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,17 +9,22 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error: null
         }
         componentDidMount() {
-            axios.interceptors.request.use( req => {
+            this.requestInterceptor = axios.interceptors.request.use( req => {
                 this.setState({
                     error: null
                 });
                 return req;
             })
-            axios.interceptors.response.use(res => res, err => {
+            this.responseInterceptor = axios.interceptors.response.use(res => res, err => {
                 this.setState({
                     error: err
                 })
             })
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.response.eject(this.responseInterceptor);
         }
 
         errorConfirmedHandler = () => {
