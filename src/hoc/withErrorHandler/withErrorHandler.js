132e96575801baf6509c03dc5e31b9ee47fe1f96
Disher Buttar <buttar.dilsher@gmail.com>
2019-06-20 21:11:10 -0700

    Handles errors with global error handler higher order function using axios

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Aux';
+
+
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends React.Component {
+        state = {
+            error: null
+        }
+        componentDidMount() {
+            axios.interceptors.request.use( req => {
+                this.setState({
+                    error: null
+                });
+                return req;
+            })
+            axios.interceptors.response.use(res => res, err => {
+                this.setState({
+                    error: err
+                })
+            })
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({
+                error: null
+            })
+        }
+
+        render() {
+            return (
+                <Aux>
+                    <Modal 
+                    clicked = {this.errorConfirmedHandler}
+                    show = {this.state.error}>
+                        {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    <WrappedComponent {...this.props}/>
+                </Aux>
+            )
+        }
+    }
+}
+
+export default withErrorHandler;
