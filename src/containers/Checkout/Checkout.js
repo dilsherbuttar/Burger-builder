08e3db363bbb5ddff5f62ddf50f37e58abf57e48
Disher Buttar <buttar.dilsher@gmail.com>
2019-07-17 15:29:49 -0700

    Submits order to the database and passed the data between pages

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import { Route } from 'react-router-dom';
-import ContactData from '../../containers/Checkout/ContactData/ContactData'
+import ContactData from '../../containers/Checkout/ContactData/ContactData';
 
 
 class Checkout extends Component {
     state = {
         ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
-        }
+            
+        },
+        price: 0
     }
-    componentDidMount() {
+    componentWillMount() {
      
         const query = new URLSearchParams(this.props.location.search);
-        const ingredients = {}
+        const ingredients = {};
+        let price = 0;
         for (let param of query.entries()) {
-            ingredients[param[0]] = +param[1]
+            if(param[0] === 'price') {
+                price = param[1];
+            } else {
+
+                ingredients[param[0]] = +param[1]
+            }
         }
+        delete ingredients.price
         this.setState({
-            ingredients: ingredients
+            ingredients: ingredients,
+            price: price
         })
+        console.log(this.state)
     }
     checkoutCancelledHandler = () => {
         this.props.history.goBack()
@@ -38,7 +45,8 @@ class Checkout extends Component {
                 checkoutCancelled = {this.checkoutCancelledHandler}
                 checkoutContinued = {this.checkoutContinuedHandler}
                 />
-                <Route path = {this.props.match.path + '/contact-data'} component = {ContactData}/>
+                <Route path = {this.props.match.path + '/contact-data'} render = { (props) => (<ContactData ingredients = {this.state.ingredients} price = {this.state.price} {...props}/>)}/>
+                {/* instead of component we used render so that we can pass the props  */}
                 
             </div>
         )
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,33 +1,76 @@
-import React, { Component } from 'react';
-import Button from '../../../components/UI/Button/Button';
-import classes from './ContactData.css';
+import React, { Component } from "react";
+import Button from "../../../components/UI/Button/Button";
+import classes from "./ContactData.css";
+import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner'
 
 class ContactData extends Component {
-    state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
-        }
-    }
-    orderHandler = () => {
+  state = {
+    name: "",
+    email: "",
+    address: {
+      street: "",
+      postalCode: ""
+    },
+    loading: false
+  };
+  orderHandler = e => {
+    e.preventDefault();
+    this.setState({
+      spinner: true
+    });
 
-    }
-    render() {
-        return (
-            <div className = {classes.ContactData}>
-                <h4>Enter your data here</h4>
-                <form>
-                    <input type = "text" name = "name" placeholder = "Your name"></input>
-                    <input type = "email" name = "email" placeholder = "Your email"></input>
-                    <input type = "text" name = "street" placeholder = "Street"></input>
-                    <input type = "text" name = "postal" placeholder = "Postal"></input>
-                    <Button btnType = "Success" clicked = {this.orderHandler}>ORDER</Button>
-                </form>
-            </div>
-        )
-    }
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: "Dilsher Buttar",
+        address: {
+          street: "500 El Camino Real",
+          zipCode: 94010,
+          country: "USA"
+        },
+        email: "buttar.dilsher@gmail.com"
+      },
+      delivery: "fastest"
+    };
+    console.log(order);
+    axios
+      .post("/orders.json", order)
+      .then(response => {
+        this.setState({
+          spinner: false,
+          purchasing: false
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          spinner: false,
+          purchasing: false
+        });
+      });
+  };
+  render() {
+      let form = (<form>
+        <input type="text" name="name" placeholder="Your name" />
+        <input type="email" name="email" placeholder="Your email" />
+        <input type="text" name="street" placeholder="Street" />
+        <input type="text" name="postal" placeholder="Postal" />
+        <Button btnType="Success" clicked={this.orderHandler}>
+          ORDER
+        </Button>
+      </form>);
+      if (this.state.loading) {
+          form = <Spinner />
+      }
+    return (
+      <div className={classes.ContactData}>
+        <h4>Enter your data here</h4>
+        {form}
+      </div>
+    );
+  }
 }
 
 export default ContactData;
