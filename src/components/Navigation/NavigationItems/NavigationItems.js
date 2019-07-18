bad7468245ab88fe4bd07bf9acdebeab33040acf
Disher Buttar <buttar.dilsher@gmail.com>
2019-07-18 15:46:06 -0700

    Implements navigation links for router

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,8 +5,8 @@ import classes from './NavigationItems.css';
 function navigationItems() {
     return (
         <ul className = {classes.NavigationItems}>
-           <NavigationItem link = "/" active>Burger Builder</NavigationItem>
-           <NavigationItem link = "/">Checkout</NavigationItem>
+           <NavigationItem link = "/" >Burger Builder</NavigationItem>
+           <NavigationItem link = "/orders">Orders</NavigationItem>
            <NavigationItem></NavigationItem>
         </ul>
     )
