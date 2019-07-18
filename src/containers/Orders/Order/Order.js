76a65425536a3da7e6f61c13f132177fdc573f4f
Disher Buttar <buttar.dilsher@gmail.com>
2019-07-18 15:56:45 -0700

    Fetches orders from Firebase

--- src/containers/Orders/Order/Order.js
+++ src/containers/Orders/Order/Order.js
@@ -2,7 +2,9 @@ import React from 'react';
 import classes from './Order.css';
 
 
+
 function Order(props) {
+    
     return (
         <div className = {classes.Order}>
             <p>Meat 1 Other ingridients</p>
